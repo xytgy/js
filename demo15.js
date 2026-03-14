532dc48a38f38a72993a4c94691bb13913ef5826
xytgy <xytgy99@qq.com>
2026-03-14 10:03:12 +0800

    新增 demo15.js 和 demo16.js 文件，包含基本的字符串示例和多行文本支持

---
+++ demo15.js
@@ -0,0 +1,6 @@
+let str1 = 'hello';      // 单引号
+let str2 = "world";      // 双引号
+let str3 = `template`;   // 模板字符串
+console.log(str1); // 输出: hello
+console.log(str2);
+console.log(str3);
---
+++ demo16.js
@@ -0,0 +1,7 @@
+let name = "Alice";
+let msg = `Hello, ${name}!`;   // 插入变量
+let multi = `多行
+文本`;                         // 支持换行
+console.log(msg);   // 输出: Hello, Alice!
+console.log(multi); // 输出: 多行
+                   //       文本
